3a90f68dc8ed945a2b3dc466494c0a98137d75b0
柳田琉成 <iesuyryanagita@gmail.com>
2025-04-20 15:01:59 +0900

    change componetnts.d.ts

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TopForm: typeof import('./src/components/TopForm.vue')['default']

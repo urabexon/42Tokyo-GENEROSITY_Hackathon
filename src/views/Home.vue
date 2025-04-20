b33c7da4385dba61058b012b119854e486a7c8b9
柳田琉成 <iesuyryanagita@gmail.com>
2025-04-20 10:19:35 +0900

    update:ok multi condition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,6 +17,13 @@ import TopList from '@/components/TopList.vue';
 
 const items = ref<any[]>([]);
 const statusFilter = ref('全て'); // フィルタ用のステータス
+const jobFilter = ref('全て')
+
+const clearFilters = () => {
+  statusFilter.value = '全て'
+  jobFilter.value = '全て'
+}
+
 
 // タスクデータを取得する関数
 const fetchData = async () => {
@@ -29,10 +36,11 @@ onMounted(fetchData);
 
 // フィルタリングされたタスクリストを返すcomputedプロパティ
 const filteredItems = computed(() => {
-  if (statusFilter.value === '全て') {
-    return items.value;
-  }
-  return items.value.filter(item => item.status === statusFilter.value);
+  return items.value.filter(item => {
+    const matchStatus = statusFilter.value === '全て' || item.status === statusFilter.value;
+    const matchJob = jobFilter.value === '全て' || jobFilter.value === ' ' || item.currentJob === jobFilter.value;
+    return matchStatus && matchJob;
+  });
 });
 
 // 新しいタスクを追加する関数
@@ -66,7 +74,16 @@ const handleDelete = async (id) => {
       class="mx-auto"
       style="max-width: 300px"
     />
-
+    <v-combobox
+    v-model="jobFilter"
+    :items="['GENEROSITY', 'MIXI', 'REAZON HOLDINGS', 'aws']"
+    label="今の職場で絞り込み（自由入力OK）"
+    class="mx-auto"
+    style="max-width: 300px"
+    />
+    <v-btn @click="clearFilters" color="primary" class="mt-4">
+    検索条件をクリア
+    </v-btn>
     <!-- 件数表示 -->
     <div class="text-center mb-4">
       <div>{{ filteredItems.length }}件見つかりました</div>
